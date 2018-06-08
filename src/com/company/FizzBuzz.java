98fcbd945deca1921e6f59169af1a68321652e15
Josemy <josemyjoseph@gmail.com>
2018-06-08 09:31:24 -0400

    This won't take long version of FizzBuzz

--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -1,25 +1,43 @@
 package com.company;
-// Shazam version of FizzBuzz
 
+// This won't take long version of FizzBuzz
 import java.util.Random;
 
 public class FizzBuzz {
+
     static Random rand = new Random();
-    public static void main(String[] args) {number1to100();
+    static int randStart, randEnd;
+    public static void main(String[] args) {
+        getcrazyFizzBuzz();
     }
+    private static void getcrazyFizzBuzz() {
+        randStart = 1 + rand.nextInt(101);
+        randEnd = 1 + rand.nextInt(101);
+        if(randEnd<randStart) {
+            System.out.println(randStart);
+            System.out.println(randEnd);
+            System.out.println("Ending number is less than staring number. Please try again!");
+        }else if(randStart == randEnd) {
+            System.out.println("Shazam!!");
+        }else {
+            // getDigits() method checks number of digits
+            getDigits();
+            // Call the getFizzBuzz() method
+            getFizzBuzz();
+        }
+    }
+    private static void getDigits() {
+        int numDigits = randEnd - randStart;
+        if(numDigits < 10) {
+            System.out.println("This won't take long!");
+            System.out.println("::::::::::::::::::");
+        }
 
-    // Method for findig the multiples
-    private static void number1to100() {
-        int randStart = 1 + rand.nextInt(101);
-        int randEnd = 1 + rand.nextInt(101);
-        System.out.println(randStart);
-        System.out.println(randEnd);
-
+    }
+    private static void getFizzBuzz() {
         for(int i = randStart; i <= randEnd; i++) {
             int val = i;
-            if(randStart == randEnd) {
-                System.out.println("Shazam!!");
-            }else if((i%3==0) && (i%5==0)) {
+            if((i%3==0) && (i%5==0)) {
                 System.out.println("FizzBuzz");
             }else if(i%3==0) {
                 System.out.println("Fizz");
